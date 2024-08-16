ed3d8cafeb484a033592a1879f6bbffa19dbb1b2
Ravi Pratihast <ravipratihast@gmail.com>
2024-08-16 21:12:34 +0530

    Add: Dashboard Added.

---
+++ src/Page/Dashboard/Dashboard.jsx
@@ -0,0 +1,48 @@
+import Navbar from "../../Components/Navbar/Navbar";
+import { IoMdAdd } from "react-icons/io";
+import { LuRefreshCw } from "react-icons/lu";
+import { PiDotsThreeVerticalBold } from "react-icons/pi";
+import { GoClockFill } from "react-icons/go";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import Categories from "../../Components/Categories/Categories";
+import Drawer from "../../Components/Drawer/Drawer";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="dashboard">
+        <div className="dashboard-container">
+          <h2 className="dashboard-title">CNAPP Dashboard</h2>
+          <div className="dashboard-buttons">
+            <button className="add-widget-button" onClick={toggleDrawer}>
+              Add Widget <IoMdAdd />
+            </button>
+            <button className="dashboard-icon-button">
+              <LuRefreshCw />
+            </button>
+            <button className="dashboard-icon-button">
+              <PiDotsThreeVerticalBold />
+            </button>
+            <button className="dashboard-date-button">
+              <GoClockFill /> | Last 2 days <RiArrowDropDownLine />
+            </button>
+          </div>
+        </div>
+        <div className="categories-container">
+          <Categories />
+        </div>
+        <Drawer isOpen={isDrawerOpen} onClose={toggleDrawer} />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
