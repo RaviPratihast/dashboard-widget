8ca5877a9ea3291a9d1b1e2e8fa4a4a2cf99de0a
Ravi Pratihast <ravipratihast@gmail.com>
2024-08-16 21:11:30 +0530

    Add: Category Component.

---
+++ src/Components/Categories/Categories.jsx
@@ -0,0 +1,13 @@
+import Widget from "../Widget/widget";
+const Categories = () => {
+  return (
+    <div className="category-container-1">
+      <h3>CSPM Executive Dashboard</h3>
+      <div className="widgets-container">
+        <Widget />
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
