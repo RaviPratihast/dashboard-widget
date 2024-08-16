aad75c044d1248937a99311f3050b729f6193c1e
Ravi Pratihast <ravipratihast@gmail.com>
2024-08-16 21:11:56 +0530

    Add: Widget added.

---
+++ src/Components/Widget/Widget.jsx
@@ -0,0 +1,12 @@
+import { IoMdAdd } from "react-icons/io";
+const Widget = () => {
+  return (
+    <div className="widget widget-1">
+      <button className="add-widget-button">
+        <IoMdAdd /> Add Widget
+      </button>
+    </div>
+  );
+};
+
+export default Widget;
