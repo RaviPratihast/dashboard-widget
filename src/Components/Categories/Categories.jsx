274aeda9312b7e50318b9daa8f9c61e288fadfd2
Ravi Pratihast <ravipratihast@gmail.com>
2024-08-17 21:17:21 +0530

    Add: Categories added

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,14 +1,26 @@
-import Widget from "../Widget/widget";
+import Widget from "../Widget/Widget";
 import "./Categories.css";
-const Categories = () => {
+
+const Categories = ({ category, toggleDrawer }) => {
+  // Copy the widgets array from the category
+  const widgets = [...category.widgets];
+
+  // Add empty widgets until there are 3
+  while (widgets.length < 3) {
+    widgets.push({
+      id: `add-widget-${widgets.length + 1}`,
+      name: "",
+      text: "",
+    });
+  }
+
   return (
-    <div className="category-container-1">
-      <h3>CSPM Executive Dashboard</h3>
+    <div className={`category-container-${category.id}`}>
+      <h3>{category.name}</h3>
       <div className="widgets-container">
-        <Widget />
-        <Widget />
-        <Widget />  
-         
+        {widgets.map((widget) => (
+          <Widget key={widget.id} widget={widget} toggleDrawer={toggleDrawer} />
+        ))}
       </div>
     </div>
   );
