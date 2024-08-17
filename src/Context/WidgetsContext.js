70f59de5b2f7e629683f2fd9f25e037b5dfcc26f
Ravi Pratihast <ravipratihast@gmail.com>
2024-08-17 15:20:32 +0530

    Add: Context added

---
+++ src/Context/WidgetsContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useReducer } from "react";
+import WidgetsReducer from "../Reducer/WidgetReducer";
+import initialData from "../data/data.json"; // Import JSON data
+
+// Define the initial state from JSON data
+const initialState = initialData.categories;
+
+// Create a context
+const WidgetsContext = createContext();
+const useWidget = () => useContext(WidgetsContext);
+
+const WidgetsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(WidgetsReducer, initialState);
+
+  return (
+    <WidgetsContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WidgetsContext.Provider>
+  );
+};
+
+export { WidgetsProvider, useWidget };
