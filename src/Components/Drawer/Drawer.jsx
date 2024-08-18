baec41db64f6b051b3d941fd25c0468efb6223f5
Ravi Pratihast <ravipratihast@gmail.com>
2024-08-18 21:51:47 +0530

    fit: add widget from drawer.

--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -1,6 +1,60 @@
+import { useState } from "react";
+import { useWidget } from "../../Context/WidgetsContext";
+import { v4 as uuidv4 } from "uuid"; // Import uuid for generating unique IDs
 import "./Drawer.css"; // Import the CSS file for the drawer
 
 const Drawer = ({ isOpen, onClose }) => {
+  const { state, dispatch } = useWidget();
+  const [formData, setFormData] = useState({ name: "", text: "" });
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newWidget = {
+      id: uuidv4(),
+      name: formData.name,
+      text: formData.text,
+    };
+
+    if (selectedCategoryId) {
+      dispatch({
+        type: "ADD_WIDGET",
+        payload: {
+          categoryId: selectedCategoryId,
+          widget: newWidget,
+        },
+      });
+    }
+
+    setFormData({ name: "", text: "" });
+    onClose();
+  };
+
+  const handleCategoryClick = (categoryId) => {
+    setSelectedCategoryId(categoryId);
+  };
+
+  const handleCheckboxChange = (widgetId, isChecked) => {
+    if (!isChecked) {
+      dispatch({
+        type: "REMOVE_WIDGET",
+        payload: {
+          categoryId: selectedCategoryId,
+          widgetId,
+        },
+      });
+    }
+  };
+
   return (
     <div className={`drawer-backdrop ${isOpen ? "open" : ""}`}>
       <div className="drawer">
@@ -14,22 +68,68 @@ const Drawer = ({ isOpen, onClose }) => {
           <p>Personalize your dashboard by adding the following widget</p>
           <div className="drawer-navigation">
             <ul>
-              <li className="active">CSPM</li>
-              <li>CWPP</li>
-              <li>Image</li>
-              <li>Ticket</li>
+              {state.map((category) => (
+                <li
+                  key={category.id}
+                  className={selectedCategoryId === category.id ? "active" : ""}
+                  onClick={() => handleCategoryClick(category.id)}
+                >
+                  {category.name}
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="checkbox-container">
-            <input type="checkbox" id="option1" className="custom-checkbox" />
-            <label>Cloud Accounts</label>
-          </div>
+
+          {selectedCategoryId && (
+            <div className="checkbox-container">
+              {state
+                .find((category) => category.id === selectedCategoryId)
+                .widgets.map((widget) => (
+                  <div key={widget.id} className="checkbox">
+                    <input
+                      type="checkbox"
+                      id={widget.id}
+                      checked
+                      onChange={(e) =>
+                        handleCheckboxChange(widget.id, e.target.checked)
+                      }
+                    />
+                    <label>{widget.name}</label>
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+
+        <div className="widget-create-container">
+          <label htmlFor="name">Widget Name</label>
+          <input
+            type="text"
+            id="name"
+            value={formData.name}
+            name="name"
+            onChange={handleInputChange}
+            placeholder="Enter widget name"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="text">Widget Text</label>
+          <input
+            type="text"
+            id="text"
+            value={formData.text}
+            name="text"
+            onChange={handleInputChange}
+            placeholder="Enter widget text"
+          />
         </div>
         <div className="drawer-footer">
           <button className="drawer-cancel-button" onClick={onClose}>
             Cancel
           </button>
-          <button className="drawer-confirm-button">Confirm</button>
+          <button className="drawer-confirm-button" onClick={handleSubmit}>
+            Confirm
+          </button>
         </div>
       </div>
     </div>
